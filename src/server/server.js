e0a90738c13980d0b013667c8fd84ad256c2525b
AdrianDevid <adriandevid36@gmail.com>
2024-02-15 16:49:01 -0300

    sadas

---
+++ src/server/server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+
+
+const io =  require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  // Handle chat messages
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message); // Broadcast the message to all connected clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3001, () => {
+  console.log('WebSocket server listening on port 3001');
+});
